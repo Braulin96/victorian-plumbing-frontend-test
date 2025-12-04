632bf9d9b466d0ce333bfac040dadd3360319e4f
Braulin Pires <braulinpires@hotmail.com>
2025-12-04 11:53:53 +0000

    removed unused components and storiees

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
+
 
 function App() {
 
   return (
-    <div className='p-[20px]'>
+    <div className='p-[20px] h-full flex w-full bg-white w-screen h-screen justify-center items-center flex-col gap-4'>
       <p className='text-red-200'>
         Welcome to the victorian plumbing frontend test
       </p>
