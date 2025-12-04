b23b0e29ea71f581b468566fbc05670714dfc490
Braulin Pires <braulinpires@hotmail.com>
2025-12-04 12:49:44 +0000

    created link component to be used to navigate to product url

---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+const Link = ({ href, text = 'View product' }) => {
+    return (
+        <a
+            href={`https://www.victorianplumbing.co.uk/${href}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="
+    mt-auto inline-block py-1 px-4 rounded-full border border-primary-green
+    no-underline text-[0.9rem] text-center text-primary-green
+    focus:outline-none focus-visible:ring-2 focus-visible:ring-primary-green
+    transform translate-y-0 hover:-translate-y-0.5
+    hover:bg-primary-green hover:text-white
+    transition-all duration-400
+    ${customClasses}"
+        >
+            {text}
+        </a>
+    )
+}
+
+Link.propTypes = {
+    href: PropTypes.string.isRequired,
+    text: PropTypes.string,
+}
+
+export default Link
