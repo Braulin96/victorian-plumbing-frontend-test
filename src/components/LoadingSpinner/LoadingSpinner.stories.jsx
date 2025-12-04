ae7060020f504b837a18eaaf48294cffab747dcc
Braulin Pires <braulinpires@hotmail.com>
2025-12-04 16:05:09 +0000

    created LoadingSpinner component to be used when loading returned data from api

---
+++ src/components/LoadingSpinner/LoadingSpinner.stories.jsx
@@ -0,0 +1,15 @@
+import LoadingSpinner from "./LoadingSpinner";
+
+export default {
+    title: "Example/LoadingSpinner",
+    component: LoadingSpinner,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+};
+
+export const SpinnerDefault = {
+    args: {},
+};
+
