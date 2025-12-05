1ce1a984fd524aec515ab8cb6c94fc490c3cca50
Braulin Pires <braulinpires@hotmail.com>
2025-12-05 10:47:31 +0000

    added class  line-clamp-2 to set maximum number of line to 2

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 
 const Title = ({ productName }) => {
     return (
-        <h3 className='text-[1rem] text-dark-gray font-semibold mb-[0.5rem]'
+        <h3 className='text-[1rem] text-dark-gray font-semibold mb-[0.5rem] line-clamp-2'
         >
             {productName}
         </h3>
