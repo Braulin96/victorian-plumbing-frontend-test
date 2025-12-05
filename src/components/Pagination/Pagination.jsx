87694f7a6e25ec7a0269b37240d5e0d07c3786a3
Braulin Pires <braulinpires@hotmail.com>
2025-12-05 08:53:29 +0000

    removed unecessary tag div wrapper in Pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -8,29 +8,29 @@ const Pagination = ({ page, setPage, totalPages }) => {
     return (
         <nav
             aria-label="Pagination Navigation"
-            role='navigation'>
-            <div className="flex items-center gap-[1rem] justify-center text-base [&_button]:size-5">
-                <Asset
-                    image={Arrow}
-                    alt="Previous page"
-                    variant="iconButton"
-                    onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
-                    disabled={page === 0}
-                    ariaLabel="Go to previous page"
-                    customClasses="rotate-180"
-                />
-                <span className="text-center text-dark-gray text-[1rem]">
-                    Page {page + 1} of {totalPages}
-                </span>
-                <Asset
-                    image={Arrow}
-                    alt="Next page"
-                    variant="iconButton"
-                    onClick={() => setPage((prev) => Math.min(prev + 1, totalPages - 1))}
-                    disabled={page === totalPages - 1}
-                    ariaLabel="Go to next page"
-                />
-            </div>
+            role='navigation'
+            className='flex items-center gap-[1rem] justify-center text-base [&_button]:size-5'>
+            <Asset
+                image={Arrow}
+                alt="Previous page"
+                variant="iconButton"
+                onClick={() => setPage((prev) => Math.max(prev - 1, 0))}
+                disabled={page === 0}
+                ariaLabel="Go to previous page"
+                customClasses="rotate-180"
+            />
+            <span className="text-center text-dark-gray text-[1rem]">
+                Page {page + 1} of {totalPages}
+            </span>
+            <Asset
+                image={Arrow}
+                alt="Next page"
+                variant="iconButton"
+                onClick={() => setPage((prev) => Math.min(prev + 1, totalPages - 1))}
+                disabled={page === totalPages - 1}
+                ariaLabel="Go to next page"
+            />
+
         </nav>
     )
 }
